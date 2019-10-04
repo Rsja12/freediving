9cedcd9817cf680be8443c89d677b48391a5b81a
Rsja12 <asocarras10@gmail.com>
2019-10-04 11:40:27 -0400

    Added line in scrape_descriptions to drop first 4 unnecessary elements in description arrays.

--- lib/freediving/scraper.rb
+++ lib/freediving/scraper.rb
@@ -4,22 +4,26 @@ require 'nokogiri'
 
 class Freediving::Scraper
 
-    
-
     @doc = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
     
     def self.scrape_site
-        discipline = @doc.css(".text-center h3").text.split(/(?<=APNEA)/) 
+        discipline = @doc.css(".text-center h3").text.split(/(?<=APNEA)/)
     end
     
     def self.scrape_descriptions(the_discipline)
-        description = @doc.css(".text-center p").text #holds unnecessary info at the beginning of string
+        arr = @doc.css(".text-center p").text
+        desc = arr.split(/(?=[A-Z])/).drop(4)
+        # last3 = desc.last(3)
+        # last3.join(" ")
         binding.pry 
     end
 
+    
 
 end
 
 
 # split1 = description.split(/(?=[A-Z])/) 
-# split1.drop(4)
+# split2 = split1.drop(4)
+# split3 = split2.last(3)
+# last3 = split3.join(" ")
