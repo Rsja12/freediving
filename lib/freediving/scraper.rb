bc12d145fbe9e42b8f2b2495bddd7b0521a1bf81
Rsja12 <asocarras10@gmail.com>
2019-10-04 10:50:34 -0400

    Took doc variable out of self.scrape_site and made it an instance variable, in order to simplify both methods in Scraper.rb

--- lib/freediving/scraper.rb
+++ lib/freediving/scraper.rb
@@ -4,15 +4,16 @@ require 'nokogiri'
 
 class Freediving::Scraper
 
+    
 
+    @doc = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
+    
     def self.scrape_site
-        doc = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
-        discipline = doc.css(".text-center h3").text.split(/(?<=APNEA)/) 
+        discipline = @doc.css(".text-center h3").text.split(/(?<=APNEA)/) 
     end
     
     def self.scrape_descriptions(the_discipline)
-        site = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
-        description = site.css(".text-center p").text #holds unnecessary info at the beginning of string
+        description = @doc.css(".text-center p").text #holds unnecessary info at the beginning of string
         binding.pry 
     end
 
