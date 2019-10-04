17d65d85ec3d6d0a68455b6661039a281f5dea3d
Rsja12 <asocarras10@gmail.com>
2019-10-03 20:17:53 -0400

    Added two comments to the bottom of Scraper.rb. These comments explain how to START breaking down the description arrays.

--- lib/freediving/scraper.rb
+++ lib/freediving/scraper.rb
@@ -7,15 +7,18 @@ class Freediving::Scraper
 
     def self.scrape_site
         doc = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
-        discipline = doc.css(".text-center h3").text.split(/(?<=APNEA)/)
-        
+        discipline = doc.css(".text-center h3").text.split(/(?<=APNEA)/) 
     end
     
     def self.scrape_descriptions(the_discipline)
         site = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
-        description = site.css(".text-center h3 p").text
-        # binding.pry 
+        description = site.css(".text-center p").text #holds unnecessary info at the beginning of string
+        binding.pry 
     end
 
 
 end
+
+
+# split1 = description.split(/(?=[A-Z])/) 
+# split1.drop(4)
