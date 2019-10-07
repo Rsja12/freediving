8e688f90da4f959ce7d0aac6edf5678c71f7a340
Rsja12 <asocarras10@gmail.com>
2019-10-07 18:36:05 -0400

    Completely changed scrape_site method. Now I'm iterating over an array of hashes that contains both the discipline name and the discipline description. The method now returns an array of hashes with relevant info.

--- lib/freediving/scraper.rb
+++ lib/freediving/scraper.rb
@@ -4,30 +4,21 @@ require 'nokogiri'
 
 class Scraper
 
+    attr_accessor :name, :description 
     
     def self.scrape_site
         # iterate through the array that contains both dis and desc. then make a hash with key value pairs
-        
         doc = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
-        discipline_info = doc.css(".article-main__content").css(".text-center")
-        binding.pry 
+        discipline_info = doc.css(".article-main__content .text-center")
+
+        discipline_info.collect do |element|
+            {
+                :name => element.css("h3").text,
+                :description => element.css("p").text
+            }
+        end
     end
+    binding.pry 
     
-    # def self.scrape_descriptions
-    #     arr = @doc.css(".text-center p").text
-    #     desc = arr.split(/(?=[A-Z])/).drop(4)
-    #     last3 = desc.last(3)
-    #     lastd = last3.join(" ")
-    #     new_arr = desc.shift(5)
-    #     new_arr << lastd 
-
-    #     first_part = new_arr.shift(2)
-    #     last_part = new_arr.pop(2)
-    #     middle = new_arr.join(" ")
-    #     first_part << middle
-    #     first_part << last_part
-
-    #     descriptions_array = first_part.flatten  
-    # end
-
+   
 end
