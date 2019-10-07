d9ceea78bd9d3916a7e53ac1df654cfd293e9f6b
Rsja12 <asocarras10@gmail.com>
2019-10-07 17:37:05 -0400

    About to make many changes. need to make whole app more OOP. Holding all information I need to iterate over in a variable in self.scrape_site

--- lib/freediving/scraper.rb
+++ lib/freediving/scraper.rb
@@ -4,27 +4,30 @@ require 'nokogiri'
 
 class Scraper
 
-    @doc = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
     
     def self.scrape_site
-        discipline = @doc.css(".text-center h3").text.split(/(?<=APNEA)/)
+        # iterate through the array that contains both dis and desc. then make a hash with key value pairs
+        
+        doc = Nokogiri::HTML(open("https://www.spotmydive.com/en/news/freediving-world-record-by-discipline-aida-ssi-padi"))
+        discipline_info = doc.css(".article-main__content").css(".text-center")
+        binding.pry 
     end
     
-    def self.scrape_descriptions
-        arr = @doc.css(".text-center p").text
-        desc = arr.split(/(?=[A-Z])/).drop(4)
-        last3 = desc.last(3)
-        lastd = last3.join(" ")
-        new_arr = desc.shift(5)
-        new_arr << lastd 
+    # def self.scrape_descriptions
+    #     arr = @doc.css(".text-center p").text
+    #     desc = arr.split(/(?=[A-Z])/).drop(4)
+    #     last3 = desc.last(3)
+    #     lastd = last3.join(" ")
+    #     new_arr = desc.shift(5)
+    #     new_arr << lastd 
 
-        first_part = new_arr.shift(2)
-        last_part = new_arr.pop(2)
-        middle = new_arr.join(" ")
-        first_part << middle
-        first_part << last_part
+    #     first_part = new_arr.shift(2)
+    #     last_part = new_arr.pop(2)
+    #     middle = new_arr.join(" ")
+    #     first_part << middle
+    #     first_part << last_part
 
-        descriptions_array = first_part.flatten  
-    end
+    #     descriptions_array = first_part.flatten  
+    # end
 
 end
